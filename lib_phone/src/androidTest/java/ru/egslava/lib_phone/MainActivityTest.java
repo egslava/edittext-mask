49d719f3d8ddfcfe2e46d5676df30be6a02d1014
Viacheslav Egorenko <egslava@gmail.com>
2017-03-04 10:35:44 +0700

    Testing of +7(999)705-56-71

---
+++ lib_phone/src/androidTest/java/ru/egslava/lib_phone/MainActivityTest.java
@@ -0,0 +1,60 @@
+package ru.egslava.lib_phone;
+
+
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.test.suitebuilder.annotation.LargeTest;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class MainActivityTest {
+
+    public static final int phone_input = br.com.sapereaude.maskedEditText.test.R.id.vControl;
+    @Rule
+    public ActivityTestRule<TestActivity> mActivityTestRule = new ActivityTestRule<>(TestActivity.class);
+
+    @Test
+    public void textTypingTest() {
+        onView(
+            allOf(
+                withId(phone_input),
+                withText("+7(   )   -  -  "),
+                isDisplayed())
+        ).perform(click());
+
+        // just in case, check again that it's the same view after the click
+        onView(
+            allOf(
+                withId(phone_input),
+                withText("+7(   )   -  -  "),
+                isDisplayed())
+        ).perform(typeText("9997055671"));
+
+        onView(
+            allOf(
+                withId(phone_input),
+                withText("+7(999)705-56-71"),
+                isDisplayed())
+        ).perform(closeSoftKeyboard());
+
+        onView( allOf(
+            withId(phone_input),
+            isDisplayed())
+        ).check(matches(withText("+7(999)705-56-71")));
+
+    }
+}
---
+++ lib_phone/src/androidTest/java/ru/egslava/lib_phone/TestActivity.java
@@ -0,0 +1,16 @@
+package ru.egslava.lib_phone;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by egslava on 04/03/2017.
+ */
+
+public class TestActivity extends AppCompatActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(br.com.sapereaude.maskedEditText.test.R.layout.activity_main);
+    }
+}
