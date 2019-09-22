171583114d5b11ba26406fca3a211f3d5a4a3ba9
Slava Egorenkov <egslava@gmail.com>
2019-09-22 23:05:39 +0300

    1. A proof that setHint doesn't work (new checkbox in the TestActivity). 2. enable_ime_action demonstration (really)
    Basically it was a temp commit and I'm not sure that I'm doing it right. The objective - I want all changes to be commited before @kounalem (Emmanouil) is gonna make a move. He is going to resolve the issue #25 (https://github.com/egslava/edittext-mask/issues/25 )

--- MaskedEditText/src/androidTest/java/ru/egslava/lib_phone/TestActivity.java
+++ MaskedEditText/src/androidTest/java/ru/egslava/lib_phone/TestActivity.java
@@ -2,15 +2,34 @@ package ru.egslava.lib_phone;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+
+import br.com.sapereaude.maskedEditText.MaskedEditText;
+
+import static br.com.sapereaude.maskedEditText.test.R.*;
 
 /**
  * Created by egslava on 04/03/2017.
  */
 
-public class TestActivity extends Activity {
+public class TestActivity extends Activity implements CompoundButton.OnCheckedChangeListener {
+
+    private MaskedEditText maskedEditText;
+    private CheckBox vcbKeepHint;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(br.com.sapereaude.maskedEditText.test.R.layout.activity_main);
+        setContentView(layout.activity_main);
+        vcbKeepHint = findViewById(id.keep_hint);
+        maskedEditText = findViewById(id.vControl);
+        vcbKeepHint.setOnCheckedChangeListener(this);
+    }
+
+    @Override
+    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+        maskedEditText.setKeepHint(vcbKeepHint.isChecked());
     }
 }
